29e2affe77bbef0aea9b0fa3acbe61289bebd635
HuyDung <nguyenhdung2006@gmail.com>
2026-06-09 23:37:39 +0700

    Initialize Spring Boot backend structure

---
+++ backend/src/main/java/com/huydung/devarchive/DevarchiveApplication.java
@@ -0,0 +1,13 @@
+package com.huydung.devarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevarchiveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/huydung/devarchive/DevarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huydung.devarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevarchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
